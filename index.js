547da09f6de7566f9be3a79d87567880cc882877
EDB02 <69971062+EDB02@users.noreply.github.com>
2023-08-31 10:40:45 +0200

    fix app

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ import app from './app.js'
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, "0.0.0.0", () => console.log(`Super frigo listening on ${port}!`));
+app.app.listen(port, "0.0.0.0", () => console.log(`Super frigo listening on ${port}!`));
